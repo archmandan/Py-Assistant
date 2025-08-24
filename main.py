e09eedd3240146652f602d5119b9e969fa73f20c
archmandan <dan.thedev@outlook.com>
2025-08-24 16:35:15 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-import speech_recognition as sr
+from pynput import keyboard # type: ignore
 import tkinter as tk
+from tkinter import messagebox
+import speech_recognition as sr
 
-# == Function for recognising speech ==
-def listening():
+# == Listening ==
+def micListening():
+    listen_button.config(text="🎙️ Listening...", command="None")
+
     r = sr.Recognizer()
     mic = sr.Microphone()
 
-# == Main GUI Code and program code ==
-def main():
-    root = tk.Tk()
-    root.geometry("300x300")
-    root.title("")
+    listen_button.config(text="🎙️ Listen", command=micListening)
 
-    root.mainloop()
+# == Check if key is Copilot key ==
+def checkKey(key):
+    if key == keyboard.Key.f23:
+        msg = messagebox.showinfo("IAN", "Listening started")
+        micListening()
 
-if __name__ == "__main__":
-    main()
+# == Window Setup ==
+root = tk.Tk()
+root.geometry("300x200")
+root.title("IAN")
+
+# == Widgets ==
+listen_button = tk.Button(root, text="🎙️ Listen", command=micListening)
+
+# == Layout ==
+listen_button.grid(column=1, row=1)
+
+root.mainloop()
